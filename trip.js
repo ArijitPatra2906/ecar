6ae74a5107106beda18b68eecd1daaf48fd9c1ac
Arijit Patra <patraarijit440@gmail.com>
2022-10-30 14:40:25 +0530

    trip model

--- trip.js
+++ trip.js
@@ -1,24 +1,24 @@
 const plugin = ({ widgets, simulator, vehicle }) => {
     const trip = document.createElement("div")
+    const tripCount = document.createElement("h5")
+    tripCount.innerHTML = Math.floor((Math.random() * 100)) + "km/hr"
     trip.setAttribute("style", `height: 100%; width: 100%;`)
     trip.innerHTML = (`
     <div id="tripBox" style="max-width: fit-content; margin: 0 auto; position: relative;>
     <i class="fa-sharp fa-solid fa-location-dot"></i>
     <h3>Trip distance</h3>
-    <h5 id="tripcount">143KM</h5>
+    <h5 id="tripcount"></h5>
 </div>
     `)
 
 
-    const tripCount = document.getElementById("tripcount")
 
 
 
     setInterval(count, 3000);
 
-    function count() {
-        tripCount.innerHTML = Math.floor((Math.random() * 100)) + "km/hr"
-    }
+
+
 
     let boxGlobal = null
 
@@ -41,3 +41,4 @@ const plugin = ({ widgets, simulator, vehicle }) => {
 
 export default plugin
 
+trip.js
