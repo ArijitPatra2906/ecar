7740345cb990ddc4417cca7f325a46b5be33bac2
Arijit Patra <patraarijit440@gmail.com>
2022-10-30 11:00:42 +0530

    trip model

--- box.js
+++ box.js
@@ -2,15 +2,33 @@ const plugin = ({ widgets, simulator, vehicle }) => {
     const container = document.createElement("div")
     container.setAttribute("style", `height: 100%; width: 100%;`)
     container.innerHTML = (`
-    <div style="max-width: fit-content; margin: 0 auto; position: relative;">
-    <img src="https://firebasestorage.googleapis.com/v0/b/digital-auto.appspot.com/o/media%2FDashboardPhone.png?alt=media&token=d361018a-b4b3-42c0-8ef0-16c9e70fd9c7" style="height: 100%; width: 100%; object-fit: contain;">
-        <div class="smartphone-text" style="position: absolute; color: white; font-family: 'Lato'; width: 100%; top: 0; height: 100%; box-sizing: border-box; padding-top: 25px; padding-right: 12px; padding-left: 12px; padding-bottom: 25px;"></div>
-    </div>
+    <div class="trip">
+    <i class="fa-sharp fa-solid fa-location-dot"></i>
+    <h6>Trip distance</h6>
+    <h5 id="tripcount"></h5>
+</div>
     `)
+
+
+    // const speedCount = document.getElementById("speedcount")
+    const tripCount = document.getElementById("tripcount")
+    // const batteryCount = document.getElementById("batterycount")
+    // const fuelCount = document.getElementById("fuelcount")
+
+
+
+    setInterval(count, 3000);
+
+    function count() {
+        // speedCount.innerHTML = Math.floor((Math.random() * 100)) + "km/hr"
+        tripCount.innerHTML = Math.floor((Math.random() * 100)) + "km/hr"
+        // batteryCount.innerHTML = Math.floor((Math.random() * 100)) + "%"
+        // fuelCount.innerHTML = Math.floor((Math.random() * 100)) + "%"
+    }
 
     let boxGlobal = null
 
-    widgets.register("Image", (box) => {
+    widgets.register("TRIP", (box) => {
         boxGlobal = box
         box.injectNode(container)
         return () => {
