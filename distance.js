97b3b88bdb128867f9e4be550c9a3919df7e19ff
Arijit Patra <patraarijit440@gmail.com>
2022-10-30 21:10:50 +0530

    trip model

--- distance.js
+++ distance.js
@@ -1,9 +1,9 @@
 const plugin = ({ widgets, simulator, vehicle }) => {
-    const trip = document.createElement("div")
+    const container = document.createElement("div")
     const tripCount = document.createElement("h5")
     tripCount.innerHTML = setInterval(Math.floor((Math.random() * 100)) + "km/hr", 3000)
-    trip.setAttribute("style", `height: 100%; width: 100%;`)
-    trip.innerHTML = (`
+    container.setAttribute("style", `height: 100%; width: 100%;`)
+    container.innerHTML = (`
     <div class="tripBox" style="max-width: fit-content; margin: 0 auto; position: relative;text-align:center">
     <i class="fa-sharp fa-solid fa-location-dot"></i>
     <h3>Trip distance</h3>
@@ -15,7 +15,7 @@ const plugin = ({ widgets, simulator, vehicle }) => {
 
     widgets.register("TRIP", (box) => {
         boxGlobal = box
-        box.injectNode(trip)
+        box.injectNode(container)
         box.injectNode(tripCount)
         return () => {
             boxGlobal = null
@@ -25,7 +25,7 @@ const plugin = ({ widgets, simulator, vehicle }) => {
     return {
         set_text: (text) => {
             if (boxGlobal !== null) {
-                trip.querySelector(".tripcount").textContent = text
+                container.querySelector(".tripcount").textContent = text
             }
         }
     }
